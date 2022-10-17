546ff9174b67772b2d2d90af9bed041cd718fa04
mani682 <manichandanamiriyala682@gmail.com>
2022-10-17 10:08:17 +0530

    addressbook

--- Program.cs
+++ Program.cs
@@ -7,7 +7,8 @@
             //Console.WriteLine("Welcome to my addressbook program");
             //uc1_addressbook.PrintPerson();
             //uc2_addressbook.Display();
-            uc3_addressbook.EditContact();
+            //uc3_addressbook.EditContact();
+            uc4_addressbook.DeleteContact();
             
         }
     }
