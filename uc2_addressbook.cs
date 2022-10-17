f38d2d1a7737fb65dff7c67d06bd74970630dae1
mani682 <manichandanamiriyala682@gmail.com>
2022-10-17 09:43:34 +0530

    display the contacts

---
+++ uc2_addressbook.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace addressbook
+{
+    public static class uc2_addressbook
+    {
+        public static void Display()
+        {
+            foreach (var contact in people)
+            {
+                Console.WriteLine($"Firstname : {contact.Firstname}\t Lastname : {contact.Lastname}\t PhoneNumber : {contact.PhoneNumber}\t city : {contact.city}\t Zip : {contact.Zip}\t Email : {contact.Email}\t Adress : {contact.Adress}\n ");
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
