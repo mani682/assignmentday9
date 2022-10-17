f773c2fc020d7bf556cc7faac72fae643412690a
mani682 <manichandanamiriyala682@gmail.com>
2022-10-17 10:07:53 +0530

    delete contact

---
+++ uc4_addressbook.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace addressbook
+{
+    public static class uc4_addressbook
+    {
+        public static void DeleteContact()
+        {
+            Console.WriteLine("Enter the first name you want to edit");
+            string Firstname = Console.ReadLine();
+
+            //int index = people.IndexOf(Firstname);
+            int index = -1;
+            for (int i = 0; i < people.Count; i++)
+            {
+                if (people[i].Firstname.ToLower() == Firstname.ToLower())
+                {
+                    index = i;
+                    break;
+                }
+            }
+
+            if (index >= 0)
+            {
+                people.RemoveAt(index);
+                Console.WriteLine("Person deleted successfully");
+                Console.ReadLine();
+
+            }
+            else
+            {
+                Console.WriteLine("No such person exist in database.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
+    }
+}
