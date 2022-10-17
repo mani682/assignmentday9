b401c6a428cddf7fc829a40fad574091019e0cc2
mani682 <manichandanamiriyala682@gmail.com>
2022-10-17 08:26:16 +0530

    addressbook

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace addressbook
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to my addressbook program");
+            uc1_addressbook.PrintPerson();
+            
+        }
+    }
+}
